57f6c54bcf9f0308879b195304c38ae54a4b1c8e
Michael Patrick <memep3@gmail.com>
2021-06-13 20:59:04 -0700

    Example script for plotting a csv. Can't handle duplicate x values

---
+++ MatPlotExample.py
@@ -0,0 +1,57 @@
+#Michael Patrick
+#6/13/21
+#Matplot from CSV example
+#Takes a CSV value, formatted as specified below,
+#and generates a plot using MatPlotLib.
+
+import csv
+import matplotlib.pyplot as plt
+import numpy
+
+if __name__ == '__main__':
+    with (open('ExampleCSV.csv', newline='')) as csvfile:
+        csvreader = csv.reader(csvfile, delimiter = ' ', quotechar='|')
+
+        x_axis = []
+        y_axis = []
+
+
+
+        #print data from csv file, add data to arrays
+        firstRow = False
+        for row in csvreader:
+            split_line = row[0].split(',')
+            #Expects three columns: name, length, height
+            print(f'{split_line[0]:15} {split_line[1]:15} {split_line[2]:15}')
+
+            if(firstRow):
+                #Also, add to x & y_axis respectively
+                x_axis.append(float(split_line[1]))
+                y_axis.append(float(split_line[2]))
+            firstRow = True
+
+        #Add data to dictionary to sort
+        #As of python 3.7, dictionaries are ordered!
+        axisDict = {}
+        for i in range(len(x_axis)):
+            axisDict[x_axis[i]] = y_axis[i]
+
+        #Now that it's sorted into a relationship, add data back
+        #to axis lists
+        sorted(axisDict)
+        x_axis.clear()
+        y_axis.clear()
+        index = 0;
+        for x_value in axisDict.keys():
+            if (index != 0):
+                x_axis.append(x_value)
+                y_axis.append(axisDict[x_value])
+            index += 1
+
+        print("Post splitting....")
+        print(x_axis)
+        print(y_axis)
+
+        #plot with matplotlib
+        plt.scatter(x_axis, y_axis)
+        plt.show()
